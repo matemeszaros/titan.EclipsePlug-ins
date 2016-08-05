2ab685962d4e4769ffa3ca6546c05534a7206555
kristof <Kristof.Szabados@ericsson.com>
2016-08-05 14:07:39 +0200

    getShell could rturn null even on graphical display.
    Signed-off-by: kristof <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/wizards/projectFormat/NewTITANProjectImportWizard.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/wizards/projectFormat/NewTITANProjectImportWizard.java
@@ -17,6 +17,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.titan.common.logging.ErrorReporter;
 import org.eclipse.ui.IImportWizard;
 import org.eclipse.ui.IWorkbench;
@@ -36,7 +37,7 @@ public class NewTITANProjectImportWizard extends BasicNewResourceWizard implemen
 	private TpdImporter tpdImporter;
 
 	public NewTITANProjectImportWizard() {
-		tpdImporter = new TpdImporter(getShell(), false);
+		tpdImporter = new TpdImporter(Display.getDefault().getActiveShell(), false);
 	}
 
 	@Override
