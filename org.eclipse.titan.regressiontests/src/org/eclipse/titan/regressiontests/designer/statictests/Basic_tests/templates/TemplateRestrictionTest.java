510b33510cef3a3746f7688cc2fc61c268421c9d
balaskoa <jeno.balasko@ericsson.com>
2016-08-09 16:44:50 +0200

    template restriction test is added to the regression test
    Signed-off-by: balaskoa <jeno.balasko@ericsson.com>

---
+++ org.eclipse.titan.regressiontests/src/org/eclipse/titan/regressiontests/designer/statictests/Basic_tests/templates/TemplateRestrictionTest.java
@@ -0,0 +1,51 @@
+package org.eclipse.titan.regressiontests.designer.statictests.Basic_tests.templates;
+
+import java.util.ArrayList;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.titan.regressiontests.designer.Designer_plugin_tests;
+import org.eclipse.titan.regressiontests.library.MarkerToCheck;
+import org.junit.Test;
+//import org.junit.Ignore;
+
+//@Ignore("The tested feature is not ready yet")
+public class TemplateRestrictionTest {
+	//templateRestrictionTest_ttcn
+	@Test
+	public void templateRestrictionTest_ttcn() throws Exception {
+		Designer_plugin_tests.checkSemanticMarkersOnFile(templateRestrictionTest_ttcn_initializer_errors(), "src/Basic_tests/templates/templateRestrictionTest.ttcn");
+		Designer_plugin_tests.checkSemanticMarkersOnFile(templateRestrictionTest_ttcn_initializer_warnings(), "src/Basic_tests/templates/templateRestrictionTest.ttcn");
+	}
+
+	private ArrayList<MarkerToCheck> templateRestrictionTest_ttcn_initializer_errors() {
+		//templateRestrictionTest.ttcn
+		ArrayList<MarkerToCheck> markersToCheck = new ArrayList<MarkerToCheck>(8);
+		int lineNum = 29;
+		int i = 0;
+		for (i = 0; i < 2; i++) {
+			markersToCheck.add(new MarkerToCheck("Formal parameter with template restriction `present' not allowed here", lineNum, IMarker.SEVERITY_ERROR));
+		}
+		lineNum += 22;
+		markersToCheck.add(new MarkerToCheck("Restriction 'value' on reference template does not allow usage of this template",  lineNum, IMarker.SEVERITY_ERROR));
+		lineNum += 6;
+		markersToCheck.add(new MarkerToCheck("Restriction 'value' on template parameter does not allow usage of this template",  lineNum, IMarker.SEVERITY_ERROR));
+		lineNum += 2;
+		for (i = 0; i < 2; i++) {
+			markersToCheck.add(new MarkerToCheck("Restriction 'value' on template parameter does not allow usage of this template", lineNum++, IMarker.SEVERITY_ERROR));
+		}
+		markersToCheck.add(new MarkerToCheck("Restriction 'value' on template parameter does not allow usage of this template",  ++lineNum, IMarker.SEVERITY_ERROR));
+		lineNum += 2;
+		markersToCheck.add(new MarkerToCheck("Restriction 'present' on template variable does not allow usage of `ifpresent'",  lineNum, IMarker.SEVERITY_ERROR));
+
+		return markersToCheck;
+	}
+	
+	private ArrayList<MarkerToCheck> templateRestrictionTest_ttcn_initializer_warnings() {
+		//templateRestrictionTest.ttcn
+		ArrayList<MarkerToCheck> markersToCheck = new ArrayList<MarkerToCheck>(1);
+		int lineNum = 51;
+		markersToCheck.add(new MarkerToCheck("Inadequate restriction on the referenced template `t_f', this may cause a dynamic test case error at runtime",  lineNum, IMarker.SEVERITY_WARNING));
+
+		return markersToCheck;
+	}
+}
--- org.eclipse.titan.regressiontests/src/org/eclipse/titan/regressiontests/designer/statictests/Basic_tests/templates/TemplateTestSuite.java
+++ org.eclipse.titan.regressiontests/src/org/eclipse/titan/regressiontests/designer/statictests/Basic_tests/templates/TemplateTestSuite.java
@@ -12,6 +12,6 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AllFromTest.class })
+@SuiteClasses({ AllFromTest.class, TemplateRestrictionTest.class })
 public class TemplateTestSuite {
 }
